907d57461cb868d526904afe335e9d7825a69cb4
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-08 16:12:04 +0700

    Update and rename DevbyNongcroissantV4-luck.sh to start-luckDevV4.sh

--- DevbyNongcroissantV4-luck.sh
+++
@@ -1,1 +0,0 @@
-./DevbyNongcroissantV4 -v -l na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV4 -p x -t 12
---
+++ start-luckDevV4.sh
@@ -0,0 +1,1 @@
+chmod +x DevbyNongcroissantV4 && ./DevbyNongcroissantV4 -v -l na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV4 -p x -t 12
